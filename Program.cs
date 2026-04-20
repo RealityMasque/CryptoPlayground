ef648bf22a0b4462c29125b9d4d8722d1575aedb
Orion Culver <orionculver@hotmail.com>
2026-04-20 16:04:26 +0100

    Initial CryptoPlayground: key generation, signing, verification

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+using RSA rsa = RSA.Create(2048);
+
+string publicKey = rsa.ToXmlString(false);
+string privateKey = rsa.ToXmlString(true);
+
+Console.WriteLine($"Public Key: {publicKey}");
+Console.WriteLine();
+Console.WriteLine($"Private Key: {privateKey}");
+Console.WriteLine();
+
+string message = "Hello, World!";
+byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+byte[] signature = rsa.SignData(messageBytes, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+
+Console.WriteLine($"Signature: {Convert.ToBase64String(signature)}");
+Console.WriteLine();
+
+bool isValid = rsa.VerifyData(messageBytes, signature, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+Console.WriteLine($"Is the signature valid? {isValid}");
+Console.WriteLine();
+
